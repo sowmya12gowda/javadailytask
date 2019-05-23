eb1b62d056e596f850d84456471f9b1f139c1baf
sowmya <Sningegowdaostb1@gmail.com>
2019-05-23 09:39:32 +0530

    daily task

---
+++ task2jss/js/java.js
@@ -0,0 +1,10 @@
+var massJohn=50;
+var heightJohn=2.7;
+var massMark=60;
+var heightMark=6.5;
+var BMIMark=50/(2.7*2.7);
+console.log(BMIMark);
+var BMIJohn=60/(6.5*6.5);
+console.log(BMIJohn);
+var highermarksBMI=BMIMark>BMIJohn;
+console.log("Is Mark's BMI higher than John's?" +highermarksBMI);
